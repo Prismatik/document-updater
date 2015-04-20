d71f42583617ffc0e07948160e0daf1f1ddace1a
Simon Taylor <nizmox@gmail.com>
2015-04-20 17:26:10 +1000

    WIP

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ var safeParse = require('safe-json-parse/callback');
 //publish document-migrator to npm
 
 var documentUpdater = {
+  //to store schemas for each version of the document, property = version number (as string), value = the json schema itself (not parsed)
   schemas: {},
-  currentVersion: 1,
 
   init: function(schemaPath, migrationPath, cb) {
-    this.getSchemas(schemaPath, cb);
+    this.fetchSchemas(schemaPath, cb);
   },
 
   //fetch the schemas from the files in the specified schema directory
   //populate the schemas dictionary AND update the currentVersion (most recent schema version)
-  getSchemas: function(schemaPath, cb) {
+  fetchSchemas: function(schemaPath, cb) {
     var self = this;
 
     //loop through all the schemas in the specified folder and populate the scheams object
@@ -25,30 +25,51 @@ var documentUpdater = {
         fs.readFile(schemaPath + "/" + filename, 'utf-8', function (err, schema) {
           if (err) return callback(err);
 
-          safeParse(schema, function(err, parsedSchema) {
-            if (err) return callback(err);
+          console.log('filename', filename);
+          var versionRegex = /-v([0-9])*\./
+          var version = versionRegex.exec(filename);
 
-            //save the json schema (not parsed) against the version number
-            self.schemas[parsedSchema.version] = schema;
-
-            var versionInt = parseInt(parsedSchema.version);
-            if (versionInt > self.currentVersion) self.currentVersion = parsedSchema.version;
-            return callback();
-          });
+          if (version && version[1]) {
+            var verNo = version[1]
+            console.log('version #', verNo);
+            //version = parseInt(version);
+            self.schemas[version[1]] = schema;
+            callback(null, schema);
+          } else {
+            var err = new Error("Invalid schema doc filename, must be in the format name-vX.json, where X = the schema version");
+            callback(err);
+          }
         });
       }, function(err, result) {
         console.log('schemas',self.schemas);
-        console.log('currentVersion',self.currentVersion);
-        console.log('current schema',self.schemas['2']);
+        //console.log('currentVersion',self.currentVersion);
+        //console.log('current schema',self.schemas['2']);
         cb();
       });
 
     });
-  }
+  },
 
-  //updateDocument: function(do) {
-  
-  //}
+  getLatestVersion: function() {
+    var self = this;
+    var versions = Object.keys(self.schemas);
+    versions = versions.map(function(version) { return parseInt(version); })
+    console.log('test',versions)
+    return Math.max(versions);
+  },
+
+  updateDocument: function(doc, cb) {
+    var self = this;
+    var latest = self.getLatestVersion();
+    console.log('latest',latest);
+    if (doc.version < self.currentVersion) {
+      console.log('update needed!');
+    }
+    cb(null, doc);
+    //first get document version
+    //compare this version to the current version
+    //if the document version is lesser, apply each required update
+  }
 }
 
 module.exports = documentUpdater;
--- test/index.js
+++ test/index.js
@@ -1,10 +1,25 @@
+var assert = require('assert');
 var documentUpdater = require('../index.js');
 
 
 describe('document-updater', function() {
-  it('.getSchemas', function(done) {
-    console.log('Running tests');
-    //define where the schema and migrations live for this document type
-    documentUpdater.getSchemas('./test/schema', done);
+  describe('.fetchSchemas', function() {
+    it('must do stuff', function(done) {
+      console.log('Running tests');
+      //define where the schema and migrations live for this document type
+      documentUpdater.fetchSchemas('./test/schema', done);
+    });
+  });
+  describe('.updateDocument', function() {
+    it('must work', function(done) {
+      var doc = {
+        version: 1
+      }
+
+      documentUpdater.updateDocument(doc, function(err, updated) {
+        assert.deepEqual(updated, {version: 2});
+        done();
+      });
+    });
   });
 });
